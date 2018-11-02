ea066c4cabe93f37fb0552c02605f26c6d3b55c0
Predrag <idmpepe@gmail.com>
2018-11-02 17:23:30 +0100

    Refactor Hover

--- features/hover.py
+++ features/hover.py
@@ -10,6 +10,7 @@ from SIDE.features.lib.helpers import get_word, defintion
 class SideHover(sublime_plugin.ViewEventListener):
     def on_hover(self, point, hover_zone):
         scope_name = self.view.scope_name(point)
+        # not hovering over text
         if hover_zone != sublime.HOVER_TEXT:
             return
 
@@ -23,77 +24,102 @@ class SideHover(sublime_plugin.ViewEventListener):
         word = get_word(self.view, point)
         locations = defintion(word, self.view)
 
-        definition_count = len(locations)
-
-        if definition_count > 1:
+        # display the reference count
+        if len(locations) > 1:
             content = """
-                <body id="side-hover" style="margin:0">
-                    <div style="color: color(var(--foreground) alpha(0.7)); 
-                                padding: 7px;">{} definitions</div>
-                </body>""".format(definition_count)
+            <body id="side-hover" style="margin:0">
+                <div style="color: color(var(--foreground) alpha(0.7)); padding: 7px;">
+                    {} definitions
+                </div>
+            </body>""".format(len(locations))
             self.show_popup(content, point)
 
-
-        content = ''
+        # display the signiture, file origin, docs
         if len(locations) == 1:
-            location = locations[0]
-            file_path, relative_file_path, row_col = location
-            row = row_col[0]
-            find_comment_params = {'file_path': file_path, 'above_function': row - 1} 
+            file_path, relative_file_path, row_col = locations[0]
+            row, _col = row_col  # signiture row
 
+            get_docs_params = {
+                'file_path': file_path, 
+                'row_above_signiture': row - 1,
+                'row_below_signiture': None
+            } 
             signiture = linecache.getline(file_path, row).strip()
+
             if is_class:
-                while '{' not in signiture and ':' not in signiture:
-                    row += 1
-                    signiture += ' ' + linecache.getline(file_path, row).strip()
+                signiture, row = self._build_up_class_signiture(signiture, 
+                                                                file_path, 
+                                                                row)
             else:
-                while ')' not in signiture:
-                    row += 1
-                    signiture += ' ' + linecache.getline(file_path, row).strip()
-            find_comment_params['below_function'] = row + 1 
+                signiture, row = self._build_up_function_signiture(signiture, 
+                                                                   file_path,  
+                                                                   row)
+            # prettify signiture
             signiture = signiture.strip('{').strip(':')
 
-            docs = self.get_docs(find_comment_params)  
+            get_docs_params['row_below_signiture'] = row + 1 
+            docs = self._get_docs(get_docs_params)  
             if docs:
                 docs = """
-                    <div style="padding: 7px; 
-                                border-top: 1px solid color(var(--foreground) alpha(0.1))">
-                                {}
-                    </div>""".format(docs)
-            
+                <div style="padding: 7px; 
+                            border-top: 1px solid color(var(--foreground) alpha(0.1))">
+                    {}
+                </div>""".format(docs)
+
+            # prettyfy file origin
             if len(relative_file_path) > 70:
                 relative_file_path = '...' + relative_file_path[-50:]
             
-            ref = """
-                <div style="padding: 7px; 
-                            border-top: 1px solid color(var(--foreground) alpha(0.1));
-                            color: color(var(--foreground) alpha(0.7))">
-                    {}
-                </div>""".format(relative_file_path)
+            origin = """
+            <div style="padding: 7px; 
+                        border-top: 1px solid color(var(--foreground) alpha(0.1));
+                        color: color(var(--foreground) alpha(0.7))">
+                {}
+            </div>""".format(relative_file_path)
 
             content = """
             <body id="side-hover" style="margin:0">
                 <div style="color: var(--orangish); 
-                            padding: 7px;">{}</div>
-                {} {}
-            </body>""".format(signiture, ref, docs)
+                            padding: 7px;">
+                    {}
+                </div>
+                {} 
+                {}
+            </body>""".format(signiture, origin, docs)
+
             self.show_popup(content, point)
             linecache.clearcache()
 
     def show_popup(self, content, point):
         self.view.show_popup(content, sublime.HIDE_ON_MOUSE_MOVE_AWAY, location=point, max_width=700)
-    
-    def get_docs(self, find_comment_params):
+
+    def _build_up_class_signiture(self, signiture, file_path, row):
+        while '{' not in signiture and ':' not in signiture:
+            row += 1
+            signiture += ' ' + linecache.getline(file_path, row).strip()
+        return signiture, row
+
+    def _build_up_function_signiture(self, signiture, file_path, row):
+        while ')' not in signiture:
+            row += 1
+            signiture += ' ' + linecache.getline(file_path, row).strip()
+        return signiture, row
+
+    def _get_docs(self, find_comment_params):
         file_path = find_comment_params['file_path']
-        row = find_comment_params['above_function']
-        docs = linecache.getline(file_path, find_comment_params['above_function']).strip()
+
+        # extract docs above function
+        row = find_comment_params['row_above_signiture']
+        docs = linecache.getline(file_path, find_comment_params['row_above_signiture']).strip()
+        # go in reverse
         if '*/' in docs:
             while '/*' not in docs:
                 row -= 1
                 docs = linecache.getline(file_path, row).strip() + '<br>' + docs
             return docs
 
-        row = find_comment_params['below_function']
+        # extract docs below function
+        row = find_comment_params['row_below_signiture']
         docs = linecache.getline(file_path, row).strip()
         if '"""' in docs:
             while not re.match('""".*?"""', docs,  re.MULTILINE):
