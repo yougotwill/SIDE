733c221f03d54aa32b078b2e72a2ccbd766af7e7
Predrag <idmpepe@gmail.com>
2018-11-02 16:45:47 +0100

    Refactor Goto Definition and Jump Back

--- features/definition.py
+++ features/definition.py
@@ -10,7 +10,7 @@ history = []
 
 class SideJumpBack(sublime_plugin.TextCommand):
     def run(self, edit):
-        if len(history):
+        if len(history) > 0:
             bookmark = history.pop()
             file_path, row_col = bookmark 
             row, col = row_col
@@ -23,9 +23,7 @@ class SideJumpBack(sublime_plugin.TextCommand):
 class SideDefinition(sublime_plugin.TextCommand):
     def run(self, edit):
         window = sublime.active_window()
-        cursor_pos = self.view.sel()[0].begin()
         word = get_word(self.view)
-
         locations = defintion(word, self.view)
 
         if len(locations) == 0:
@@ -33,39 +31,42 @@ class SideDefinition(sublime_plugin.TextCommand):
             return
 
         if len(locations) == 1:
-            window = sublime.active_window()
-            location = locations[0]
-            file_path, relative_file_path, row_col = location
-            row, col = row_col
-            bookmark = (self.view.file_name(), self.view.rowcol(cursor_pos))
-           
+            # save bookmark 
+            old_cursor_pos = self.view.sel()[0].begin()
+            bookmark = (self.view.file_name(), self.view.rowcol(old_cursor_pos))
             history.append(bookmark)
+
+            # open location
+            file_path, _rel_file_path, row_col = locations[0]
+            row, col = row_col
             window.open_file("{}:{}:{}".format(file_path, row, col), sublime.ENCODED_POSITION)
         
         if len(locations) > 1:
-            definitions = []
-            full_path_definitions = []
+            quick_panel = {
+                'labels': [],
+                'on_select': []
+            }
+
             for location in locations:
                 file_path, relative_file_path, row_col = location
                 row, col = row_col
-                definitions.append("{}:{}:{}".format(relative_file_path, row, col))
-                full_path_definitions.append("{}:{}:{}".format(file_path, row, col))
-
+                quick_panel['labels'].append("{}:{}:{}".format(relative_file_path, row, col))
+                quick_panel['on_select'].append("{}:{}:{}".format(file_path, row, col))
             
-            preview_view = None
             def _on_done(index):
                 if index == -1:
                     window.focus_view(self.view)
                     return 
-                file_path =  full_path_definitions[index]
-                window.open_file("{}".format(file_path), sublime.ENCODED_POSITION)
+                file_path_row_col =  quick_panel['on_select'][index]
+                window.open_file("{}".format(file_path_row_col), sublime.ENCODED_POSITION)
 
             def _on_change(index):                    
-                file_path =  full_path_definitions[index]
-                preview_view = window.open_file("{}".format(file_path), sublime.ENCODED_POSITION | sublime.TRANSIENT)
+                file_path_row_col = quick_panel['on_select'][index]
+                preview_view = window.open_file("{}".format(file_path_row_col), 
+                                                sublime.ENCODED_POSITION | sublime.TRANSIENT)
 
             window.show_quick_panel(
-                definitions,
+                quick_panel['labels'],
                 _on_done,
                 on_highlight=_on_change
             )
