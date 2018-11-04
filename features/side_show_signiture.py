30e43a5a8b7ff8a641fc776b2e1bcd2a98d02082
Predrag <idmpepe@gmail.com>
2018-11-04 10:45:24 +0100

    Fix show definition count at cursor position

--- features/side_show_signiture.py
+++ features/side_show_signiture.py
@@ -29,7 +29,7 @@ class SideShowSigniture(sublime_plugin.TextCommand):
                     {} definitions
                 </div>
             </body>""".format(len(locations))
-            self.view.show_popup(content, point)
+            self.view.show_popup(content, location=point)
             return
 
         file_path, relative_file_path, row_col = locations[0]
