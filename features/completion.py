55b269d390a7e93e19b3a806bab4c88f23539cba
Predrag <idmpepe@gmail.com>
2018-11-04 19:13:47 +0100

    dont show completions in strings

--- features/completion.py
+++ features/completion.py
@@ -7,14 +7,16 @@ from SIDE.features.lib.helpers import find_symbols
 
 class SideCompletion(sublime_plugin.ViewEventListener):
     def on_query_completions(self, prefix, locations):
-        window = sublime.active_window()
         point = locations[0]
-        views = window.views()
-
-        symbols = find_symbols(self.view, views)
-
-        completions = []
+        is_irrelevant = self.view.match_selector(point, 'string.quoted')
+        if is_irrelevant:
+            return None
         
+        window = sublime.active_window()
+        views = window.views()
+        
+        symbols = find_symbols(self.view, views)
+        completions = []
         for symbol_location in symbols:
             file_name, base_file_name, region, symbol, symbol_type = symbol_location
             completion_item = ["{}\t{}{}".format(symbol, base_file_name, symbol_type), "{}($1)$0".format(symbol)]
