3cf1bc42a4234ef43edb2126e9fa52190abc91d7
Predrag <idmpepe@gmail.com>
2018-11-04 15:07:47 +0100

    add symbol type

--- features/completion.py
+++ features/completion.py
@@ -3,10 +3,11 @@ import os
 import sublime
 import sublime_plugin
 
-def transform_to_location(file_path, symbol, region):
+def transform_to_location(file_path, region, symbol, symbol_type):
+    ''' return a tuple (file_path, base_file_name, region, symbol) '''
     file_name = os.path.basename(file_path)
     base_file_name, file_extension = os.path.splitext(file_name)
-    return (file_path, base_file_name, region, symbol)
+    return (file_path, base_file_name, region, symbol, symbol_type)
     
 
 class SideCompletion(sublime_plugin.ViewEventListener):
@@ -15,27 +16,33 @@ class SideCompletion(sublime_plugin.ViewEventListener):
         window = sublime.active_window()
         point = locations[0]
         views = window.views()
-        
-
 
         symbols = []  # List[location]
         for view in views:
             locations = view.indexed_symbols()
             for location in locations:
                 region, symbol = location
+                scope_name = view.scope_name(region.begin())
 
-                location = transform_to_location(view.file_name(), symbol, region)
+                symbol_type = '[?]'
+                if 'function' in scope_name and 'class' in scope_name:
+                    symbol_type = '[m]'  # method
+                elif 'class' in scope_name:
+                    symbol_type = '[c]'  # class
+                else:
+                    symbol_type = '[f]'  # function
+                
+                location = transform_to_location(view.file_name(), region, symbol, symbol_type)
                 symbols.append(location)
 
-        filename, file_extension = os.path.splitext(self.view.file_name())
-        print("filename", filename)
+        _file_name, file_extension = os.path.splitext(self.view.file_name())
         symbols = _locations_by_file_extension(symbols, file_extension)
-        print(symbols)
 
         completions = []
+
         for symbol_location in symbols:
-            file_name, base_file_name, region, symbol = symbol_location
-            completion_item = ["{}\t{}".format(symbol, base_file_name), "{}($1)$0".format(symbol)]
+            file_name, base_file_name, region, symbol, symbol_type = symbol_location
+            completion_item = ["{}\t{}{}".format(symbol, base_file_name, symbol_type), "{}($1)$0".format(symbol)]
             completions.append(completion_item)
 
         return completions
