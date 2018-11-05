421d6768eb43cdfb33d94ee13073a4888246cc44
Predrag <idmpepe@gmail.com>
2018-11-05 13:09:51 +0100

    Rename to jus show signatures

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from SIDE.features.side_show_signiture import SideShowSigniture
+from SIDE.features.show_signiture import SideShowSigniture
 from SIDE.features.definition import SideJumpBack, SideDefinition
 from SIDE.features.hover import SideHover
 from SIDE.features.signiture_help import SideSignitureHelp
