4aae026af1cb5437fd99715ccdddaa096a98e516
Predrag <idmpepe@gmail.com>
2018-11-06 01:51:51 +0100

    escape &qout;

--- features/commands/tell_joke.py
+++ features/commands/tell_joke.py
@@ -2,9 +2,11 @@ import sublime
 import sublime_plugin
 
 from urllib.request import Request, urlopen
+from html import escape
 
 class SideTellJoke(sublime_plugin.TextCommand):
     def run(self, edit):
         req = Request("https://geek-jokes.sameerkumar.website/api", headers={'User-Agent': 'Mozilla/5.0'})
         contents = urlopen(req).read().decode('utf-8')
-        self.view.show_popup(contents, max_width=500)
+        contents = contents.replace('&quot;', '\'')
+        self.view.show_popup(escape(contents, False), max_width=500)
