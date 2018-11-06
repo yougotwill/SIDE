3284c5f03637aca59048959e6e8cee58ad9a3fab
Predrag <idmpepe@gmail.com>
2018-11-06 02:11:45 +0100

    Add search web functionality

--- features/commands/search_web.py
+++ features/commands/search_web.py
@@ -1,7 +1,17 @@
+import os
 import sublime
 import sublime_plugin
+import webbrowser as wb
 
 
 class SideSearchWeb(sublime_plugin.TextCommand):
     def run(self, edit):
-        print('hehehe')
+        region = self.view.sel()[0]
+        word = self.view.substr(region)
+        if len(word) == 0:
+            word = self.view.substr(self.view.word(region.begin()))
+
+        filename, file_extension = os.path.splitext(self.view.file_name())
+        
+        search_term = "https://www.google.com/search?q={} {}".format(file_extension.strip('.'), word)
+        wb.open_new_tab(search_term)
