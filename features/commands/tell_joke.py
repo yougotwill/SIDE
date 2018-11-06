bf454fe5ff873e7bf73175d595ecd83d9f9edc69
Predrag <idmpepe@gmail.com>
2018-11-06 16:11:55 +0100

    Make tell joke async

--- features/commands/tell_joke.py
+++ features/commands/tell_joke.py
@@ -1,16 +1,24 @@
 import sublime
 import sublime_plugin
+import threading
 
 from urllib.request import Request, urlopen
 from html import escape
 
+
+def send_request(view):
+    req = Request("https://geek-jokes.sameerkumar.website/api", headers={'User-Agent': 'Mozilla/5.0'})
+    try:
+        contents = urlopen(req).read().decode('utf-8')
+    except:
+        contents = 'Without the Internet, my humor is gone :('
+    contents = contents.replace('&quot;', '\'')
+    view.show_popup(escape(contents, False), max_width=500)
+
+
 class SideTellJoke(sublime_plugin.TextCommand):
     def run(self, edit):
-        req = Request("https://geek-jokes.sameerkumar.website/api", headers={'User-Agent': 'Mozilla/5.0'})
-        try:
-            contents = urlopen(req).read().decode('utf-8')
-        except:
-            contents = 'Without the Internet, my humor is gone :('
+        t = threading.Thread(target=send_request, args=(self.view,))
+        t.start()
         
-        contents = contents.replace('&quot;', '\'')
-        self.view.show_popup(escape(contents, False), max_width=500)
+        
