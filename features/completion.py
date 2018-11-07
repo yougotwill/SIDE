2ee37ac9e62d3429e4ab723210849668c954c6d6
Predrag <idmpepe@gmail.com>
2018-11-07 23:48:17 +0100

    remove costly call

--- features/completion.py
+++ features/completion.py
@@ -20,13 +20,10 @@ class SideCompletion(sublime_plugin.ViewEventListener):
             completions.append(completion_item)
 
         for view in views:
-            file_name = os.path.basename(view.file_name())
-            file_name, _file_extension = os.path.splitext(file_name)
             hash_completions = view.extract_completions(prefix)
             for completion in hash_completions:
                 completion_item = ["{}\t{}".format(completion, '[#]'), "{}".format(completion)]
                 if completion_item in completions:
-                    print('completion', completion)
                     continue
                 completions.append(completion_item)
 
