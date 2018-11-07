a93eddc01d71627d21e0da17b8265f030e933251
Predrag <idmpepe@gmail.com>
2018-11-07 11:03:48 +0100

    Add words from all opend windows to completions

--- features/completion.py
+++ features/completion.py
@@ -19,4 +19,15 @@ class SideCompletion(sublime_plugin.ViewEventListener):
             completion_item = ["{}\t{}{}".format(symbol, base_file_name, symbol_type), "{}".format(symbol)]
             completions.append(completion_item)
 
+        for view in views:
+            file_name = os.path.basename(view.file_name())
+            file_name, _file_extension = os.path.splitext(file_name)
+            hash_completions = view.extract_completions(prefix)
+            for completion in hash_completions:
+                completion_item = ["{}\t{}".format(completion, '[#]'), "{}".format(completion)]
+                if completion_item in completions:
+                    print('completion', completion)
+                    continue
+                completions.append(completion_item)
+
         return completions
