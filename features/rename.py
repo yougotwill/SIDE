089ba3c2800e42431352e9cf45c55bf1eb6c8e6b
Predrag <idmpepe@gmail.com>
2019-02-19 22:26:32 +0100

    👌 IMPROVE: Rename don't call expensive function

--- features/rename.py
+++ features/rename.py
@@ -1,12 +1,12 @@
 import sublime
 import sublime_plugin
 
-from SIDE.features.lib.helpers import scroll_to_not_visible_region, get_word_regions
+from SIDE.features.lib.helpers import scroll_to_not_visible_region
 
 
 class SideRename(sublime_plugin.TextCommand):
     def run(self, edit):
-        word_regions = get_word_regions(self.view)        
+        word_regions = self.view.get_regions('side_highlight')      
 
         sel = self.view.sel()
         if len(word_regions) > 0:
