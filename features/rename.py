e6fb2e95e00a75618d7410c18446e96b3c5d6a24
Predrag <idmpepe@gmail.com>
2019-02-19 23:11:19 +0100

    Revert "👌 IMPROVE: Rename don't call expensive function"
    This reverts commit 089ba3c2800e42431352e9cf45c55bf1eb6c8e6b.

--- features/rename.py
+++ features/rename.py
@@ -1,12 +1,12 @@
 import sublime
 import sublime_plugin
 
-from SIDE.features.lib.helpers import scroll_to_not_visible_region
+from SIDE.features.lib.helpers import scroll_to_not_visible_region, get_word_regions
 
 
 class SideRename(sublime_plugin.TextCommand):
     def run(self, edit):
-        word_regions = self.view.get_regions('side_highlight')      
+        word_regions = get_word_regions(self.view)        
 
         sel = self.view.sel()
         if len(word_regions) > 0:
