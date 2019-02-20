754a6fa36b1a5b14f6297d01032190e794aca7c7
Predrag <idmpepe@gmail.com>
2019-02-20 10:45:54 +0100

    👌 IMPROVE: Rename don't scroll to bottom when invoked for first time

--- features/rename.py
+++ features/rename.py
@@ -9,10 +9,12 @@ class SideRename(sublime_plugin.TextCommand):
         word_regions = get_word_regions(self.view)        
 
         sel = self.view.sel()
+        if len(sel) > 1:
+            scroll_to_not_visible_region(word_regions, self.view)
+
         if len(word_regions) > 0:
             sel.clear()
             sel.add_all(word_regions)
-            scroll_to_not_visible_region(word_regions, self.view)
 
 
        
