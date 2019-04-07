a719fe623d934f01a7c9f5d78bfaaed7ddba57c1
Predrag <idmpepe@gmail.com>
2019-04-07 11:02:42 +0200

    📦 NEW: Add files and folder to completions

--- features/completion.py
+++ features/completion.py
@@ -2,7 +2,7 @@ import os
 import sublime
 import sublime_plugin
 
-from SIDE.features.lib.helpers import find_symbols, find_references
+from SIDE.features.lib.helpers import find_symbols, find_references, debug
 from SIDE.features.indexer import panel_state
 
 
@@ -11,9 +11,46 @@ class SideCompletion(sublime_plugin.ViewEventListener):
         global panel_state
         point = locations[0] 
 
+        completions = []
+
+        is_in_string = self.view.match_selector(point, "string.quoted")
+        if is_in_string:
+            # get whole string region
+            string_region = self.view.extract_scope(point)
+            # trim from string start to current point, the `+ 1` stripes the first `"` or `'` 
+            trimmed_region = sublime.Region(string_region.begin() + 1, point)
+
+            search_term = self.view.substr(trimmed_region)
+            # look for relative file search
+            if './' in search_term:
+                current_file_name = self.view.file_name()
+                current_folder = os.path.dirname(current_file_name)
+
+                # searched_folder = './abc/dfg/dsad'
+                searched_folder = os.path.join(current_folder, search_term)
+                last_slash_index = searched_folder.rfind('/')
+                # searched_folder = './abc/dfg'
+                searched_folder = searched_folder[:last_slash_index]
+
+                try:
+                    files_and_folders = sorted(os.listdir(searched_folder))
+                    print(files_and_folders)
+                    for f in files_and_folders:
+                        is_file = os.path.isfile(os.path.join(searched_folder, f))
+                        completion_type = '[FILE]'
+                        if not is_file: 
+                            completion_type = '[FOLDER]'
+                        completion_item = ["{}\t{}".format(f, completion_type), "{}".format(f)]
+                        completions.append(completion_item)
+
+                    completions = sorted()
+                    return (completions, sublime.INHIBIT_WORD_COMPLETIONS)
+                except Exception as e:
+                    debug('Can\'t figure out listdir:\n', e)
+
         views = self.sort_views_by_relevance()
         symbols = find_symbols(self.view, views)
-        completions = []
+
         # easy way to filter out hash completions
         unique_symbols = []
         for symbol_location in symbols:
@@ -80,3 +117,15 @@ class SideCompletion(sublime_plugin.ViewEventListener):
                     'disable_auto_insert': True,
                     # 'next_completion_if_showing': False
                 })
+
+
+class SideAutoShowFilePathCompletions(sublime_plugin.ViewEventListener):
+    def on_modified_async(self):
+        point = self.view.sel()[0].begin()
+        last_char = self.view.substr(point - 1)
+        scope_name = self.view.scope_name(point)
+
+        if last_char == '/' and 'string.quoted' in scope_name:
+            self.view.run_command("auto_complete", {
+                'disable_auto_insert': True,
+            })
